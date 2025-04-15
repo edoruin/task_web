134601ae94c515c99a3833f488cc11c600be5b56
Edoruin <edwinjeremiasagustinyack@gmail.com>
2025-04-14 21:04:26 -0400

    requeriments.tct fixed

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)  # permite peticiones desde GitHub Pages
 
-# URL de conexión a PostgreSQL (usa tu DATABASE_URL de Render aquí)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-# Modelo
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100))
-
-@app.route('/usuarios')
-def obtener_usuarios():
-    usuarios = Usuario.query.all()
-    return jsonify([{"id": u.id, "nombre": u.nombre} for u in usuarios])
+@app.route('/')
+def home():
+    return render_template('index.html')  # El archivo debe estar en la carpeta "templates"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
